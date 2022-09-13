9ecf012fddca4c39ea8dbbbad7931861c4af9592
Jeronimo <jeronatan@hotmail.com>
2022-09-13 19:27:46 -0300

    first commit

---
+++ productos.js
@@ -0,0 +1,31 @@
+const items = document.getElementById(`items`)
+const templateCard = document.getElementById(`template-card`).content
+const fragment = document.createDocumentFragment();
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    fetchData()
+})
+
+const fetchData = async () => {
+    try{
+        const res = await fetch('data.json');
+        const data = await res.json();
+        pintarCards(data);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
+const pintarCards = data => {
+    data.forEach(product => {
+        templateCard.querySelector('h3').textContent = product.productName;
+        templateCard.querySelector('p').textContent = '$' + product.price;
+        templateCard.querySelector('img').setAttribute("src", product.image);
+        const clone = templateCard.cloneNode(true);
+        fragment.appendChild(clone);
+    })
+    items.appendChild(fragment);
+}
